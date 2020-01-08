5a3b4a4762b31c2ef63cfd46f788f5325d05c0fc
shyfullahsadi <shyfullahsadi@outlook.com>
2020-01-08 12:43:30 +0600

    Active Box self project

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(function () {
+  // Team Part Slider
+  $('.team-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+  });
+
+  // Testimonial Part Slider
+  // $('.mmm').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   arrows: false,
+  //   autoplaySpeed: 2000,
+  // });
+})

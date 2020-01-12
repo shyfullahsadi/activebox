87b4903ef64987519bbc0bdb3572f9e88f717f2d
shyfullahsadi <shyfullahsadi@outlook.com>
2020-01-12 15:38:02 +0600

    responsive upto Team part, adding nagigation menus, adding back to top scroll

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,53 @@
 $(function () {
   // Team Part Slider
   $('.team-slider').slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     arrows: false,
     autoplaySpeed: 2000,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 
   // Testimonial Part Slider
-  // $('.mmm').slick({
-  //   slidesToShow: 3,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   arrows: false,
-  //   autoplaySpeed: 2000,
-  // });
+  $('.testimonial-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    autoplaySpeed: 2000,
+  });
+
+  //Back To Top
+
+  $('.back_to_top').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
 })
